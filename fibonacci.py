c5efd9eb80d48c89220b2bff2ced2e3ae69a49fe
wswalker87 <scott@wscottwalker.com>
2025-11-12 21:04:36 -0600

    got the solution, but not the second way that I wanted to try

--- fibonacci.py
+++ fibonacci.py
@@ -1,4 +1,16 @@
+import functools
 def fibonacci(n):
-  pass
+  # create an array of 0 and 1 since those are the first nums.
+  # first solution starts down one line 
+  fullArray = [0, 1]
+  # loop over the array and do the math
+  counter = n
+  index = 1
+  
+  while counter > 1:
+    temp_num = fullArray[index - 1] + fullArray[index]
+    fullArray.append(temp_num)
+    counter -= 1
+    index += 1
 
-
+  return fullArray[n]
